95ef089f04d77ae266c12d16426ec916cc14fd13
krittanut <krittanat12300@gmail.com>
2025-10-14 23:17:10 +0700

    create mocking Data planets

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,19 @@
-const http = require('http')
+const http = require("http");
 
-const app = require('./app')
+const app = require("./app");
+
+const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
- 
-const server = http.createServer(app)
 
-server.listen(PORT,()=>{
-  console.log(`Listening on port ${PORT}...`)
-})
+const server = http.createServer(app);
+
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
 
 
-
+startServer()
