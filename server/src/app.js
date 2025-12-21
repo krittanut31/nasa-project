10aa20d4695a688ddb58a628c56744ce80125142
krittanut <krittanat12300@gmail.com>
2025-12-21 22:14:51 +0700

    upload file

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,26 @@
-const express = require('express')
-const cors = require('cors')
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require('morgan')
 
-const planetesRouter = require('./routes/planets/planets.router')
+const planetesRouter = require("./routes/planets/planets.router");
 
-const app = express()
+const app = express();
 
-app.use(cors({
-  origin:'http://localhost:3000'
-}))
-app.use(express.json())
-app.use(planetesRouter)
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-module.exports = app
+app.use(morgan('combined'))
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use(planetesRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+module.exports = app;
